c9713b39a3faf44aebd42742e688a8feb536d7c4
HYisen <alexhyisen@gmail.com>
2017-05-24 20:01:55 +0900

    use RecyclerView & CardView instead ListView in MsgFragment

--- app/src/main/java/net/alexhyisen/eta1/other/msg/MessageArrayAdapter.java
+++ app/src/main/java/net/alexhyisen/eta1/other/msg/MessageArrayAdapter.java
@@ -3,10 +3,10 @@ package net.alexhyisen.eta1.other.msg;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import net.alexhyisen.eta1.R;
@@ -18,63 +18,65 @@ import java.util.List;
  * Adapter used for a better ListView of Messages.
  */
 
-class MessageArrayAdapter extends ArrayAdapter<Message> {
-    private final LayoutInflater mInflater;
+class MessageArrayAdapter extends RecyclerView.Adapter<MessageArrayAdapter.MessageViewHolder> {
+    static class MessageViewHolder extends RecyclerView.ViewHolder{
+        TextView typeText;
+        TextView timeText;
+        TextView contentText;
+
+        MessageViewHolder(View itemView) {
+            super(itemView);
+            typeText = (TextView) itemView.findViewById(R.id.typeText);
+            timeText = (TextView) itemView.findViewById(R.id.timeText);
+            contentText = (TextView) itemView.findViewById(R.id.contentText);
+        }
+    }
+
     private final Context mContext;
-    //Here, must be bind to the correct layout resource that fits createViewFromResource().
-    private static final int mResource= R.layout.list_item;
 
+    private List<Message> data;
 
-    MessageArrayAdapter(@NonNull Context context, @NonNull List<Message> objects) {
-        super(context, mResource, objects);
-        mContext = context;
-        mInflater = LayoutInflater.from(context);
+    MessageArrayAdapter(Context mContext, List<Message> data) {
+        this.mContext = mContext;
+        this.data = data;
     }
 
-    @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        //super.getView(position, convertView, parent);
-        return createViewFromResource(mInflater, position, convertView, parent, mResource);
+    public MessageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        return new MessageViewHolder(v);
     }
 
-    //copied and modified from ArrayAdapter.createViewFromResource
-    private @NonNull View createViewFromResource(@NonNull LayoutInflater inflater, int position,
-                                                 @Nullable View convertView, @NonNull ViewGroup parent, int resource) {
-        final View view =
-                (convertView == null ? inflater.inflate(resource, parent, false) : convertView);
-        final TextView typeText=(TextView) view.findViewById(R.id.typeText);
-        final TextView timeText=(TextView) view.findViewById(R.id.timeText);
-        final TextView contentText=(TextView) view.findViewById(R.id.contentText);
-        final Message item = getItem(position);
-        assert item != null;
-
-        switch (item.getType()) {
+    @Override
+    public void onBindViewHolder(MessageViewHolder holder, int position) {
+        switch (data.get(position).getType()) {
             case INFO:
-                typeText.setText(R.string.msg_type_info);
-                //mContext.getColor() is what I want, but it need API 23.
-                typeText.setTextColor(mContext.getColor(R.color.colorMsgInfo));
+                holder.typeText.setText(R.string.msg_type_info);
+                holder.typeText.setTextColor(mContext.getColor(R.color.colorMsgInfo));
                 break;
             case ERROR:
-                typeText.setText(R.string.msg_type_error);
-                typeText.setTextColor(mContext.getColor(R.color.colorMsgError));
+                holder.typeText.setText(R.string.msg_type_error);
+                holder.typeText.setTextColor(mContext.getColor(R.color.colorMsgError));
                 break;
             case SERVER:
-                typeText.setText(R.string.msg_type_server);
-                typeText.setTextColor(mContext.getColor(R.color.colorMsgServer));
+                holder.typeText.setText(R.string.msg_type_server);
+                holder.typeText.setTextColor(mContext.getColor(R.color.colorMsgServer));
                 break;
             case CLIENT:
-                typeText.setText(R.string.msg_type_client);
-                typeText.setTextColor(mContext.getColor(R.color.colorMsgClient));
+                holder.typeText.setText(R.string.msg_type_client);
+                holder.typeText.setTextColor(mContext.getColor(R.color.colorMsgClient));
                 break;
             default:
-                typeText.setText(R.string.msg_type_undefined);
+                holder.typeText.setText(R.string.msg_type_undefined);
         }
 
-        timeText.setText(item.getTime());
+        holder.timeText.setText(data.get(position).getTime());
 
-        contentText.setText(item.getContent());
+        holder.contentText.setText(data.get(position).getContent());
+    }
 
-        return view;
+    @Override
+    public int getItemCount() {
+        return data.size();
     }
 }
--- app/src/main/java/net/alexhyisen/eta1/other/msg/MessageFragment.java
+++ app/src/main/java/net/alexhyisen/eta1/other/msg/MessageFragment.java
@@ -1,12 +1,13 @@
 package net.alexhyisen.eta1.other.msg;
 
-import android.app.ListFragment;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 
 import net.alexhyisen.eta1.R;
 
@@ -18,9 +19,14 @@ import java.util.List;
  * A Fragment that used as a list of messages.
  */
 
-public class MessageFragment extends ListFragment {
+public class MessageFragment extends Fragment {
     private List<Message> data=new ArrayList<>();
-    private ArrayAdapter<Message> adapter;
+    private RecyclerView.Adapter adapter;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Nullable
     @Override
@@ -28,8 +34,11 @@ public class MessageFragment extends ListFragment {
         View view = inflater.inflate(R.layout.list_view, container, false);
 
         adapter = new MessageArrayAdapter(view.getContext(), data);
-        adapter.setNotifyOnChange(true);//additional safety guarantee, on default true
-        setListAdapter(adapter);
+
+        RecyclerView listView = (RecyclerView) view.findViewById(R.id.list_view);
+        listView.setHasFixedSize(true);
+        listView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        listView.setAdapter(adapter);
 
         return view;
     }
