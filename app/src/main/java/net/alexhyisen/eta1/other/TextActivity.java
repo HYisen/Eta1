c5d6c5fd10ef917613cae4f108c2d9f4f30b2c72
HYisen <alexhyisen@gmail.com>
2017-05-28 17:03:38 +0900

    add scroll ability and indent in TextActivity.

--- app/src/main/java/net/alexhyisen/eta1/other/TextActivity.java
+++ app/src/main/java/net/alexhyisen/eta1/other/TextActivity.java
@@ -1,10 +1,8 @@
 package net.alexhyisen.eta1.other;
 
 import android.content.Intent;
-import android.support.v4.app.NavUtils;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -23,6 +21,12 @@ public class TextActivity extends AppCompatActivity implements ToolbarOwner{
         getSupportActionBar().setTitle((String) getIntent().getExtras().get(Intent.EXTRA_TITLE));
 
         String content = (String) getIntent().getExtras().get(Intent.EXTRA_TEXT);
+        //Maybe I should use a shared preference to store indent value.
+        String indent = "        ";
+        content = indent + content;
+        //may cost a lot of time, hope it isn't troublesome.
+        content = content.replace("\n", "\n" + indent);
+
         TextView textView = (TextView) findViewById(R.id.textView);
         textView.setText(content);
     }
@@ -31,6 +35,9 @@ public class TextActivity extends AppCompatActivity implements ToolbarOwner{
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
+                //Should I override the original meaning of UpButton, which is back to its parent?
+                //There should be no exact parent activity of this view activity,
+                //as its only usage is to show the view and then return back.
                 super.onBackPressed();
                 return true;
             default:
